bf5a43db7508df28bfcf021d307014ec9656108b
Krzysztof <drkrzysztofzieba@gmail.com>
2025-03-22 12:13:00 +0100

    navlist fix, publikuj mema przycisk

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -7,14 +7,12 @@ function Header() {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
   return (
-    <div className="relative flex items-center justify-between bg-violet-800 px-6 py-4 text-zinc-100 md:gap-2">
+    <div className="relative flex items-center justify-between bg-violet-800 px-6 py-4 text-zinc-100 min-[890px]:gap-2">
       <Logo />
       <HeaderNavList isMobileNavOpen={isMobileNavOpen} />
-      <div className="flex gap-3 text-3xl md:hidden">
+      <div className="flex gap-3 text-3xl min-[890px]:hidden">
         <div
-          className={`${
-            isMobileNavOpen ? "hidden" : ""
-          } z-10 cursor-pointer md:hidden`}
+          className={`${isMobileNavOpen ? "hidden" : ""} z-10 cursor-pointer`}
           tabIndex="0"
           role="button"
           aria-label="Otwórz menu"
--- src/ui/HeaderNavList.jsx
+++ src/ui/HeaderNavList.jsx
@@ -4,20 +4,23 @@ import Button from "./Button";
 function HeaderNavList({ isMobileNavOpen }) {
   return (
     <ul
-      className={`absolute top-0 right-0 flex h-dvh w-dvw flex-col items-center justify-center gap-8 space-y-7 bg-violet-800 text-center text-2xl duration-300 md:static md:top-auto md:h-auto md:w-auto md:translate-none md:flex-row md:space-y-0 md:bg-transparent md:text-xl ${
+      className={`absolute top-0 right-0 flex h-dvh w-dvw flex-col items-center justify-center gap-8 space-y-7 bg-violet-800 text-center text-2xl duration-300 min-[890px]:static min-[890px]:top-auto min-[890px]:h-auto min-[890px]:w-auto min-[890px]:translate-none min-[890px]:flex-row min-[890px]:space-y-0 min-[890px]:bg-transparent min-[890px]:text-xl ${
         !isMobileNavOpen ? "translate-x-full" : ""
       } `}
     >
-      <li className="transition-colors duration-300 hover:text-zinc-400 md:block">
+      <li className="transition-colors duration-300 hover:text-zinc-400 min-[890px]:block">
         <a href="#">Strona Główna</a>
       </li>
-      <li className="transition-colors duration-300 hover:text-zinc-400 md:block">
+      <li className="transition-colors duration-300 hover:text-zinc-400 min-[890px]:block">
         <a href="#">Losowy Mem</a>
       </li>
-      <li className="transition-colors duration-300 hover:text-zinc-400 md:block">
+      <li className="transition-colors duration-300 hover:text-zinc-400 min-[890px]:block">
         <a href="#">Rankingi</a>
       </li>
-      <li className="md:block">
+      <li className="transition-colors duration-300 hover:text-zinc-400 min-[890px]:block">
+        <a href="#">Publikuj</a>
+      </li>
+      <li className="min-[890px]:block">
         <Button color="green">Logowanie</Button>
 
         {/* Dla zalogowanego użytkownika ⬇️*/}
